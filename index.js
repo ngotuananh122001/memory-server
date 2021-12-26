023fdacd60e4478c776a9dc6de6d39805a055aab
ngotuananh122001 <ah.ngotuananh122001@gmail.com>
2021-12-26 13:21:12 +0700

    Setup server

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+// Thêm middleware đảm bảo request hợp lệ
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors()); // disable chính sách CORS trên chrome
+
+const CONNECTION_URL =
+    "mongodb+srv://ngotuananh:doithua2001@cluster0.h652s.mongodb.net/memoryDB?retryWrites=true&w=majority";
+const PORT = 5000;
+mongoose
+    .connect(CONNECTION_URL)
+    .then((data) => console.log("Ket noi db thanh cong, data: " + data))
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on ${5000}`))
+    )
+    .catch((err) => console.log("Ket noi DB that bai, error", err));
