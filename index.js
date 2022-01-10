064683ec7aef7149f556272678cec53dcb0f6ce7
ngotuananh122001 <ah.ngotuananh122001@gmail.com>
2022-01-11 01:08:49 +0700

    add varible in .env

--- index.js
+++ index.js
@@ -20,11 +20,10 @@ app.use(cors()); // disable chính sách CORS trên chrome
 app.use('/posts', verifyToken, postRouter);
 app.use('/auth', authRouter);
 app.use('/event', verifyToken, eventRouter);
-// const CONNECTION_URL =
-//     'mongodb+srv://ngotuananh:doithua2001@cluster0.h652s.mongodb.net/memoryDB?retryWrites=true&w=majority';
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.h652s.mongodb.net/memoryDB?retryWrites=true&w=majority`;
 
 // local mongodb
-const CONNECTION_URL = `mongodb://localhost:27017/${process.env.DB_NAME}`;
+// const CONNECTION_URL = `mongodb://localhost:27017/${process.env.DB_NAME}`;
 const PORT = process.env.PORT || 5000;
 mongoose
     .connect(CONNECTION_URL)
